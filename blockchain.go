cd660006a52f7a771453fe2245771822f9eeb7f3
Runbin Zhou <runbin1000@gmail.com>
2025-01-29 21:58:59 -0500

    added code for a simple blockchain

--- blockchain.go
+++ blockchain.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-type Block struct { // Create a block struct
+type Block struct { // Define a block struct
 	data         map[string]interface{} // creates key value, key: string, value: interface (any type)
 	hash         string
 	previousHash string
@@ -17,7 +17,7 @@ type Block struct { // Create a block struct
 	pow          int
 }
 
-type Blockchain struct { // Create a blockchain struct
+type Blockchain struct { // Define a blockchain struct
 	genesisBlock Block
 	chain        []Block
 	difficulty   int
@@ -30,7 +30,7 @@ func (b Block) calculateHash() string { // method to calculate the hash of a blo
 	return fmt.Sprintf("%x", blockHash)                                                     // Return the base 16 hash as a string
 }
 
-func (b *Block) mine(difficulty int) { // increments the pow and calc block hash until valid (# of 0s > difficulty), modifies the block -> takes a reference to the block
+func (b *Block) mine(difficulty int) { // increments the pow and calc block hash until valid (# of 0s > difficulty), modifies the block -> takes a reference to the block, *Block is a receiver type
 	for !strings.HasPrefix(b.hash, strings.Repeat("0", difficulty)) { // While the hash doesn't start with the required number of 0s
 		b.pow++                    // Increment the pow
 		b.hash = b.calculateHash() // Calculate the hash of the block
@@ -46,5 +46,50 @@ func CreateBlockchain(difficulty int) Blockchain { // Create a new blockchain wi
 		genesisBlock,          // store the genesis block as a field
 		[]Block{genesisBlock}, // add genesis block to the chain (slice of blocks)
 		difficulty,
+	}
+}
+
+func (b *Blockchain) addBlock(from, to string, amount float64) { // *Blockchain receiver type, from, to, amount -> var parameters
+	blockData := map[string]interface{}{ // Create blockData var (map) -> assign keys and values
+		"from":   from,
+		"to":     to,
+		"amount": amount,
+	}
+	lastBlock := b.chain[len(b.chain)-1] // Get the last block on the chain
+	newBlock := Block{
+		data:         blockData,
+		previousHash: lastBlock.hash,
+		timestamp:    time.Now(),
+	}
+	newBlock.mine(b.difficulty)         // Mine the new block
+	b.chain = append(b.chain, newBlock) // Append the new block to the chain
+}
+
+func (b Blockchain) isValid() bool {
+	for i := range b.chain[1:] { // Loops through chain starting from the 2nd block, assign i the indexes which represent the blocks starting from the 2nd block.
+		previousBlock := b.chain[i]                                                                               // get previous block starting from the genesis block
+		currentBlock := b.chain[i+1]                                                                              // get current block starting from the 2nd block
+		if currentBlock.hash != currentBlock.calculateHash() || currentBlock.previousHash != previousBlock.hash { // check validation
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	blockchain := CreateBlockchain(2) // create a new blockchain with a difficulty of 2
+	blockchain.addBlock("Bob", "Alice", 5)
+	blockchain.addBlock("John", "Bob", 15)
+
+	fmt.Println(blockchain.isValid())
+
+	fmt.Println("\nBlockchain Structure:")
+	for i, block := range blockchain.chain {
+		fmt.Printf("\nBlock %d:\n", i)
+		fmt.Printf("  Previous Hash: %s\n", block.previousHash)
+		fmt.Printf("  Data: %+v\n", block.data)
+		fmt.Printf("  Timestamp: %s\n", block.timestamp)
+		fmt.Printf("  PoW: %d\n", block.pow)
+		fmt.Printf("  Hash: %s\n", block.hash)
 	}
 }
